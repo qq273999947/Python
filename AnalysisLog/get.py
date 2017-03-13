919a35b5c87b26d7de84acd419f20db30ce9c2a6
hanyiming <hanyiming@baidu.com>
2017-03-13 15:54:50 +0800

    优化接口获取数据脚本

--- AnalysisLog/get.py
+++ AnalysisLog/get.py
@@ -5,19 +5,35 @@ import urllib
 import urllib2
 import sys
 import json
+import os
 
-for line in open('key'):
-    key = line.upper()
-    url = "http://..." + key
-    req = urllib2.Request(url)
+def cur_file_dir():
+    #获取当前脚本路径
+    return os.path.split(os.path.realpath(__file__))[0]
 
-    res_data = urllib2.urlopen(req)
-    res = res_data.read()
-    
-    js = json.loads(res.encode('latin-1'))
-    i = js['data']
-    l = len(i)
-    #print l
-    
-    if l > 0:
-        print res
+def printUsage():$
+    print '''python {} file_key '''.format(sys.argv[0])
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3 or (not os.path.isdir(sys.argv[1])):
+        printUsage()
+        sys.exit()
+    files = os.listdir(sys.argv[1])
+
+    f = open(cur_file_dir() + file)
+    lines = f.readlines()
+    f.close()
+
+    for line in lines:
+        line= line.strip('\n')
+        url = "http://...." + line +
+        try:
+            req = urllib2.Request(url)
+            res_data = urllib2.urlopen(req)
+            res = res_data.read()
+            js = json.loads(res.encode('latin-1'))
+            i = js[line]['data']['playcnt']
+            print line + '\t' + str(i)
+        except:
+            print line + '\t' + '0'
+            continue
